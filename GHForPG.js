f511799d58c5752df218a38162cdc716304d9405
rjsweblab <72805041+rjsweblab@users.noreply.github.com>
2021-04-17 17:54:06 +0200

    changes on-project-loaded

--- GHForPG.js
+++ GHForPG.js
@@ -48,7 +48,7 @@ $(function () {
             <ul class="dropdown-menu" id="gh-dropdown">
                 <li><a href="#" id="create-repo">Create New Repo</a></li>
                 <li><a href="#" id="clone-repo">Clone Existing Repo</a></li>
-                <hr>
+                <hr id="ruler-one">
                 <li><a href="#" id="gh-settings">Settings...</a></li>
             </ul>
         </li>
@@ -64,15 +64,16 @@ $(function () {
             let targetMenu = document.getElementById('gh-dropdown');
             let newItems = document.createDocumentFragment();
             let listOne = document.createElement('li');
-            let menuItemOne = '<a href="#" id="stage-changes">Stage Changes</a>';
-            listOne.innerHTML = menuItemOne;
+            listOne.innerHTML = '<a href="#" id="stage-changes">Stage Changes</a>';
             newItems.appendChild(listOne);
             let listTwo = document.createElement('li');
-            let menuItemTwo = '<a href="#" id="commit-changes">Commit Changes</a>';
-            listTwo.innerHTML = menuItemTwo;
+            listTwo.innerHTML = '<a href="#" id="commit-changes">Commit Changes</a>';
             newItems.appendChild(listTwo);
-            let menuDivider = targetMenu.children[2];
+            //using namedITem is more robust then using hardcoded index-number
+            // this namedItem needs an id on the element <hr> in the menu
+            let menuDivider = targetMenu.children.namedItem('ruler-one');
             targetMenu.insertBefore(newItems, menuDivider);
+            console.log(targetMenu.children);
             addAdditionalListeners();
         });
 
